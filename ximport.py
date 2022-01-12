26eec09f220b83c8f6f203ab286f61fabf21d090
Rob Barry <rob.barry@gmail.com>
2022-01-12 16:33:45 -0500

    set up BCP

--- ximport.py
+++ ximport.py
@@ -1,13 +1,24 @@
 import os
 
-from dotenv import load_dotenv
 from ximport.files import ImportFile
 from ximport.arguments import get_args
+from ximport.db import MSSQL, BCP
+from ximport.log import get_logger
 
+log = get_logger(__name__)
 
 if __name__ == "__main__":
-    load_dotenv()
     args = get_args()
-    f = ImportFile.from_args(args)
-    f.analyze()
-    print(f.rowdata.sql())
+    bcp = BCP.from_args(args)
+    f = ImportFile.from_args(args, bcp)
+    db = MSSQL.from_args(args)
+    f.process()
+    if args.create:
+        if args.drop:
+            db.cursor.execute("DROP TABLE [{}]".format(args.table))
+        statement = f.rowdata.sql(args.table)
+        log.info("Create statement: {}".format(statement))
+        db.cursor.execute(statement)
+        db.conn.commit()
+    bcp.import_file()
+    
