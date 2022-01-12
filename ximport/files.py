613fe31d01ae48f5c13881c835cae813d7d42ecc
Rob Barry <rob.barry@gmail.com>
2022-01-12 12:52:25 -0500

    create rowdata class and add chunksize

--- ximport/files.py
+++ ximport/files.py
@@ -1,82 +1,85 @@
 import csv
 
+import pandas as pd
+import numpy as np
 import attr
 
 
-@attr.s
-class AutoType:
-    name = attr.ib(default=None)
-    value = attr.ib(default=None)
-    type = attr.ib()
-    length = attr.ib()
-    level = attr.ib()
+measurer = np.vectorize(len)
 
-    @type.default
-    def type_default(self):
-        try:
-            f = float(self.value)
-            if f.is_integer():
-                return "int"
+
+@attr.s
+class RowData:
+    fields = attr.ib(factory=list)
+    types = attr.ib(factory=dict)
+    lengths = attr.ib(factory=dict)
+
+    def update(self, field, column):
+        if field not in self.fields:
+            self.fields.append(field)
+        self.update_type(field, column.dtype)
+        if self.types[field] == "object":
+            max_length = np.max(measurer(column.astype(str)))
+            self.lengths[field] = self.lengths.get(field, None)
+            if self.lengths[field] is None or self.lengths[field] < max_length:
+                self.lengths[field] = max_length
+        else:
+            self.lengths[field] = self.lengths.get(field, None)
+
+    def update_type(self, field, dtype):
+        levels = {
+            "object": 10,
+            "float64": 5,
+            "int64": 2,
+        }
+        self.types[field] = self.types.get(field, "int64")
+        if levels[str(dtype)] > levels[self.types[field]]:
+            self.types[field] = str(dtype)
+
+    def sql(self, tablename="table"):
+        sql_remap = {"int64": "int", "float64": "float", "object": "nvarchar"}
+        create = []
+        sub_create = []
+        if "." in tablename:
+            parts = tablename.split(".")
+            tablename = f"[{parts[0]}].dbo.[{parts[1]}]"
+        else:
+            tablename = f"[{tablename}]"
+        create.append(f"CREATE TABLE {tablename}")
+        for field in self.fields:
+            if self.lengths[field] is not None:
+                sub_create.append(
+                    f"[{field}] {sql_remap[self.types[field]]}({self.lengths[field]})"
+                )
             else:
-                return "float"
-        except:
-            return "varchar"
-
-    @length.default
-    def length_default(self):
-        if self.type == "varchar":
-            return len(self.value)
-        else:
-            return None
-
-
-@attr.s
-class RowTypes:
-    types = {}
-    lengths = {}
-
-    def update(self, type):
-        if type.name not in types:
-            self.types[type.name] = type
-            self.lengths[type.name] = type.length
-        else:
-            
+                sub_create.append(f"[{field}] {sql_remap[self.types[field]]}")
+        create.append("({})".format(",\n".join(sub_create)))
+        return "\n".join(create)
 
 
 @attr.s
 class ImportFile:
     filename = attr.ib()
+    chunksize = attr.ib()
+    rowcount = attr.ib(default=0)
 
     @classmethod
     def from_args(cls, args):
-        return cls(
-            filename=args.filename,
-        )
+        return cls(filename=args.filename, chunksize=args.chunksize)
 
     def analyze(self):
-        self.get_
-        with open(self.filename, "r") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                pass
+        self.rowdata = RowData()
+        for chunk in pd.read_csv(
+            self.filename,
+            chunksize=self.chunksize,
+        ):
+            self.rowcount += len(chunk)
+            self._analyze(chunk)
+
+    def _analyze(self, chunk):
+        for column in chunk.columns:
+            self.rowdata.update(column, chunk[column])
 
 
 if __name__ == "__main__":
-    import time
-
-    results = []
-    vals = {
-        "1": "int",
-        "-1": "int",
-        "1.1": "float",
-        "3.1e2": "int",
-        "franklin": "varchar",
-    }
-    for i in range(0, 10000):
-        for k, v in vals.items():
-            t0 = time.time()
-            if AutoType(k).type != v:
-                raise Exception("{} {} {}".format(k, v, AutoType(k).type))
-            results.append(time.time() - t0)
-
-    print(1000000 * (sum(results) / len(results)))
+    pass
