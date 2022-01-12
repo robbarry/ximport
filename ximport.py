c2882aaa6b0b8bac4e2a2c401e62a7af032eb275
Rob Barry <rob.barry@gmail.com>
2022-01-12 12:52:37 -0500

    adding test code

--- ximport.py
+++ ximport.py
@@ -8,4 +8,6 @@ from ximport.arguments import get_args
 if __name__ == "__main__":
     load_dotenv()
     args = get_args()
-    print(args)
+    f = ImportFile.from_args(args)
+    f.analyze()
+    print(f.rowdata.sql())
